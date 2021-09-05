c8bc7ae64bf39266f867241e8821405e2221cc14
quaaalud <86638315+quaaalud@users.noreply.github.com>
2021-09-04 20:19:20 -0500

    Update WeeklyDiscountProgram.py

--- WeeklyDiscountProgram.py
+++ WeeklyDiscountProgram.py
@@ -9,9 +9,10 @@ import pandas as pd
 discounts = pd.read_excel (r"C:\Users\Dale\Desktop\pywip\discounts1.xlsx")
 grouped1 = discounts.groupby("Submitted By")
 grouped2 = (discounts.groupby("Seller Name")).sum()
-aegis = grouped2.iloc[:1]
-nvps = grouped2.iloc[0, 2:5]
-nvps = grouped2.sum()
+aegis = grouped2.iloc[1]
+nvps0 = grouped2.iloc[0]
+nvps1 = grouped2.iloc[2:5].sum()
+nvps = nvps1 + nvps0.sum()
 
 print(aegis)
 print(nvps)
