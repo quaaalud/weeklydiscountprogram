7d4a8451b870fc8c3cf4a6ad821f9da28fd48521
quaaalud <86638315+quaaalud@users.noreply.github.com>
2021-09-13 20:14:19 -0500

    Update WeeklyDiscountProgram.py

--- WeeklyDiscountProgram.py
+++ WeeklyDiscountProgram.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
+#from pathlib import Path
+#from copy import copy
+#from typing import Union, Optional
+#import openpyxl
+#from openpyxl import load_workbook
+#from openpyxl.utils import get_column_letter
 
 ### import weekly discount file
 ### sort to lists rep and company
 ### sum rep totals and company totals providing output
 
 
-discounts = pd.read_excel (r"C:\Users\Dale\Desktop\pywip\discounts1.xlsx")
+discounts = pd.read_excel (r"C:\Users\dludwinski\Desktop\wip\All Discounts 9.5.21 to 9.11.21.xlsx")
 grouped1 = discounts.groupby("Submitted By")
 grouped2 = (discounts.groupby("Seller Name")).sum()
 aegis = grouped2.iloc[1]
@@ -17,3 +23,7 @@ print(aegis)
 print(nvps)
 print(grouped1.sum())
 print(grouped2)
+
+discount = discounts.sort_values("Seller Name", inplace=True)
+
+discounts.to_excel(r"\\nvpsserver01.nvps.local\CS_ADMIN\Weekly Discount Log\All Discounts September.xlsx", sheet_name="9.5 to 9.11", index=False)
